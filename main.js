32fa3d63de74fcc22f8bbd2339a115bb6dcb20fe
Alexei-Ruzheynikov <alexei_ruzheynikov@mail.ru>
2021-08-22 16:26:54 +0300

    prompt, confirm, alert

--- main.js
+++ main.js
@@ -6,3 +6,18 @@ function openAlert() {
 /* Пример
 многострочного
 комментария */
+// window.alert("Привет, мир!");
+// window.alert("Привет, \nмир!");
+
+// if (window.confirm("Нажмите на Ок или Отмена")) {
+//   console.log("Вы нажали ок");
+// } else {
+//   console.log("Вы нажали омтена");
+// }
+
+var UName = window.prompt("Как Вас зовут", "Noname");
+if (UName == null) {
+  alert("Пока");
+} else {
+  console.log("Привет, " + UName);
+}
