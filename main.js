cf085558de2f6c8aa20dd67ca19cab4946e02390
Alexei-Ruzheynikov <alexei_ruzheynikov@mail.ru>
2021-08-23 16:42:51 +0300

    Переменные

---
+++ main.js
@@ -0,0 +1,64 @@
+// var Num1, Num2, Str1, Str2, Str3, Bool1;
+// Num1 = 1;
+// Num2 = 2.5;
+// Str1 = "Привет";
+// Str2 = "Мир";
+// Str3 = null;
+// Bool1 = true;
+
+// document.write("<br>Тип Num1 - " + typeof Num1);
+// document.write("<br>Тип Num2 - " + typeof Num2);
+// document.write("<br>Тип Str1 - " + typeof Str1);
+// document.write("<br>Тип Str2 - " + typeof Str2);
+// document.write("<br>Тип Str3 - " + typeof Str3);
+// document.write("<br>Тип Bool1 - " + typeof Bool1);
+
+// let v1 = 1;
+// let v2 = "1";
+// let v3 = v2 + v1;
+// let v4 = v1 + v2;
+
+// var a = "1";
+// var b = parseInt(a);
+
+// var c = "5.1";
+// var d = parseFloat(c);
+
+// var e = "2 + 3";
+// var f = eval(e);
+
+// const msg = "Привет";
+// console.log(msg);
+
+// if (true) {
+//   const msg = "Привет";
+// }
+// console.log(msg);
+
+// if (true) {
+//   let a = 1;
+//   const b = 2;
+//   var c = 3;
+// }
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+// function message() {
+//   var msg = "Привет";
+//   console.log(msg);
+// }
+// message();
+// console.log(msg);
+
+// function f1() {
+//   let counter = 1;
+//   console.log(counter);
+// }
+
+// function f2() {
+//   let counter = 0;
+//   console.log(counter);
+// }
+// f1();
+// f2();
