b6ff0b642d8ed4dfb41ea5723a4302f98201b0b2
Alexei-Ruzheynikov <alexei_ruzheynikov@mail.ru>
2021-08-23 22:22:16 +0300

    Отладка и обработка ошибок

--- main.js
+++ main.js
@@ -1,68 +1,11 @@
-// function map(f, a) {
-//   var resFunc = [];
-//   var i;
-//   for (i = 0; i != a.length; i++) resFunc[i] = f(a[i]);
-//   return resFunc;
+// if (x > 10) window.alert("x>10");
+// else windows.alert("x<10");
+
+// try {
+//   window.alert("Try");
+//   t = 10;
+// } finally {
+//   window.alert("Finally");
 // }
-// var f = function (z) {
-//   return z * z * z;
-// };
-// var nums = [0, 5, 7, 9, 12];
-// var cube = map(f, nums);
-// console.log(cube);
 
-// let sum = (a, b) => a + b;
-// console.log(sum(2, 2));
-
-// function denied() {
-//   window.alert("Access Denied!");
-// }
-// denied();
-
-// function sum(x, y) {
-//   return x + y;
-// }
-// var x = sum(2, 2);
-// var a = 10;
-// var b = 12;
-// var ab = sum(a, b);
-
-//Ссылка на функцию может быть сохранена в переменную
-//Имя функции указываем без скобок
-//Вызываем функцию denied() по ссылке
-// var d = denied;
-// d();
-
-// Ссылка на анонимную функцию
-// var f = function () {
-//   window.alert("Привет");
-// };
-// f();
-
-//Ссылка на вложенную функцию
-// var x = function () {
-//   return function () {
-//     window.alert("Привет");
-//   };
-// };
-// x()();
-
-// function f_Factorial(x) {
-//   if (x == 0 || x == 1) return 1;
-//   else return x * f_Factorial(x - 1);
-// }
-// alert(f_Factorial(4));
-
-// var a = 100;
-// var b = 200;
-// function f1() {
-//   var x = 100;
-//   var b = 50;
-//   document.write("<br>a = " + a);
-//   document.write("<br>b = " + b);
-//   document.write("<br>x = " + x);
-// }
-// f1();
-// document.write("<br>");
-// document.write("a = " + a);
-// document.write("<br>b = " + b);
+// if (d == 0) throw new Error("Division by zero");
